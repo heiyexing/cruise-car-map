be35f2f7a7b9a3614b1cf77bb6c762d6627937f9
syb01094648 <syb01094648@antgroup.com>
2023-11-15 16:13:34 +0800

    fix:  重新提交代码

--- docs/home/demos/markerLayer.tsx
+++ docs/home/demos/markerLayer.tsx
@@ -1,13 +1,14 @@
 import { GaodeMap, Marker, MarkerLayer, Scene } from '@antv/l7';
 import { randomPoint } from '@turf/turf';
+import { mean } from 'lodash-es';
 import React, { useEffect, useState } from 'react';
-import { markerData } from './mock';
 
 export default () => {
   const [scene, setScene] = useState<Scene | undefined>(undefined);
   const [markerLayer, setMarkerLayer] = useState<MarkerLayer | undefined>(
     undefined,
   );
+
   useEffect(() => {
     const scene = new Scene({
       id: 'map',
@@ -15,72 +16,84 @@ export default () => {
         center: [121.438579, 31.246384],
         pitch: 0,
         zoom: 10,
-        token: '5ae4492ef912cbbc93034fea0e66ff2a',
+        style: 'normal',
       }),
     });
     setScene(scene);
-    function addMarkers() {
+
+    scene.on('loaded', () => {
       const markerLayer = new MarkerLayer({
-        // cluster: true,
+        cluster: true,
         clusterOption: {
-          element: (e) => {
+          element: (cars) => {
+            const rotation =
+              cars.properties.rotation ||
+              mean(
+                cars.properties.clusterData?.map(
+                  (item) => item.properties.rotation,
+                ),
+              ) ||
+              0;
             const el = document.createElement('div');
             el.className = `markerDivClass`;
-            el.innerHTML = `<div><img style="width: 20px;height:30px; transform: rotate(${
-              e.properties.rotation ?? 0
-            }deg); )" src='https://mdn.alipayobjects.com/huamei_k6sfo0/afts/img/A*lFnGQae3LtkAAAAAAAAAAAAADjWqAQ/original'/></div>`;
+            el.innerHTML = `<div>
+              <img 
+                style="width: 20px; height:30px; transform: rotate(${rotation}deg);" 
+                src="https://mdn.alipayobjects.com/huamei_k6sfo0/afts/img/A*lFnGQae3LtkAAAAAAAAAAAAADjWqAQ/original"
+              />
+            </div>`;
             return el;
           },
+          // 触发聚合的范围
+          radius: 40,
         },
       });
-      setMarkerLayer(markerLayer);
-      for (let i = 0; i < markerData.length; i++) {
+      const bounds = scene.getBounds();
+
+      randomPoint(100, {
+        bbox: [bounds[0][0], bounds[0][1], bounds[1][0], bounds[1][1]],
+      }).features.forEach((point) => {
+        const coordinates = point.geometry.coordinates;
         const marker = new Marker({}).setLnglat({
-          lng: markerData[i].fLon,
-          lat: markerData[i].fLat,
+          lng: coordinates[0],
+          lat: coordinates[1],
         });
-        marker.setExtData(markerData[i]);
+        marker.setExtData({
+          rotation: Math.round(Math.random() * 360),
+        });
         markerLayer.addMarker(marker);
-      }
+      });
       scene.addMarkerLayer(markerLayer);
-    }
-    scene.on('loaded', () => {
-      addMarkers();
-      scene.render();
+      setMarkerLayer(markerLayer);
     });
   }, []);
 
+  const onAddMarker = () => {
+    const bounds = scene?.getBounds();
+    if (bounds && markerLayer && scene) {
+      const point = randomPoint(1, {
+        bbox: [bounds[0][0], bounds[0][1], bounds[1][0], bounds[1][1]],
+      });
+      const coordinates = point.features[0].geometry.coordinates;
+      const marker = new Marker({}).setLnglat({
+        lng: coordinates[0],
+        lat: coordinates[1],
+      });
+      markerLayer.addMarker(marker);
+    }
+  };
+
+  const onRemoveMarker = () => {
+    const markers = markerLayer?.getOriginMarkers();
+    markerLayer?.removeMarker(markers[markers.length - 1]);
+  };
+
   return (
     <div>
-      <button
-        type="button"
-        onClick={() => {
-          const bounds = scene?.getBounds();
-          if (bounds && markerLayer && scene) {
-            const point = randomPoint(1, {
-              bbox: [bounds[0][0], bounds[0][1], bounds[1][0], bounds[1][1]],
-            });
-            const coordinates = point.features[0].geometry.coordinates;
-            const marker = new Marker({}).setLnglat({
-              lng: coordinates[1],
-              lat: coordinates[0],
-            });
-            markerLayer.addMarker(marker);
-            scene.addMarkerLayer(markerLayer);
-          }
-        }}
-      >
+      <button type="button" onClick={onAddMarker}>
         车辆+1
       </button>
-      <button
-        type="button"
-        onClick={() => {
-          const markers = markerLayer?.markers;
-          markerLayer?.removeMarker(markers[markers.length - 1]);
-          scene.addMarkerLayer(markerLayer);
-          console.log(markerLayer);
-        }}
-      >
+      <button type="button" onClick={onRemoveMarker}>
         车辆-1
       </button>
       <div id={'map'} style={{ height: 400, position: 'relative' }} />
